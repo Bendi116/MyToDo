df5b0e1101aa49c2afcffbd4ac50b641481dc81b
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-03-01 23:52:16 +0100

    Add modify(not finish yet).

--- src/dialog.js
+++ src/dialog.js
@@ -2,6 +2,7 @@ import { displayProject, refreshProject,addOptionToProjectSelection } from "./do
 import { Project ,fromNodeElemenetGetProject} from "./project-class"
 import { projectArray } from "."
 import { allSelected } from "./selection-handlerer"
+import {format} from "date-fns"
 
 
 const toDoDialog = document.querySelector("#to-do-dialog")
@@ -17,6 +18,15 @@ const projectDialog = document.querySelector("#project-dialog")
 const projectTitleInput = document.querySelector("#project-title")
 
 const toDoModDialog = document.querySelector("#to-do-mod-dialog")
+const titleModCurrentInput = document.querySelector("#title-mod-current-input")
+const describeModCurrentInput = document.querySelector("#describe-mod-current-input")
+const dateModCurrentInput = document.querySelector("#date-mod-current-input")
+const priorityModCurrentInput = document.querySelector("#priority-mod-current-input")
+const titleModInput = document.querySelector("#title-mod-input")
+const describeModInput = document.querySelector("#describe-mod-input")
+const dateModInput = document.querySelector("#date-mod-input")
+const priorityModInput = document.querySelector("#priority-mod-input")
+
 
 export function showProjectDialog(){
     const projectSubmitBtn = document.querySelector("#submit-project-btn")
@@ -52,13 +62,60 @@ export function handleToDoDialog(event, project){
 }
 
 export function showToDoModifyDialog(e){
-    console.log(e.target.parentElement.parentElement)
-    let project = fromNodeElemenetGetProject(e.target.parentElement.parentElement.parentElement.parentElement)
     const toDOModBtn = document.querySelector("#submit-to-do-mod-btn")
+    const toDoModCloseBtn = document.querySelector("#close-to-do-mod-btn")
+    let project = fromNodeElemenetGetProject(e.target.parentElement.parentElement.parentElement.parentElement)
+    let todo = project.fromNodeElementGetTodo(e.target.parentElement.parentElement)
+    titleModCurrentInput.innerText += todo.title
+    describeModCurrentInput.innerText += todo.describe
+    dateModCurrentInput.innerText += format(todo.priority,"y:L:d H:m")
+    priorityModCurrentInput.innerText += todo.priority
+
+
+    titleModInput.placeholder = todo.title
+    describeModInput.placeholder = todo.describe
+    dateModInput.placeholder = format(todo.priority,"y:L:d H:m")
+    priorityModInput.placeholder = todo.priority
+
+    
+
     toDoModDialog.showModal()
+    toDoModCloseBtn.addEventListener("click",()=>{
+        titleModInput.value =""
+        describeModInput.value = "todo.describe"
+        dateModInput.value = undefined
+        priorityModInput.value = 0
+        titleModCurrentInput.innerText = "Current Todo Title: "
+        describeModCurrentInput.innerText ="Current Todo Desc: "
+        dateModCurrentInput.innerText = "Current Todo Date: "
+        priorityModCurrentInput.innerText = "Current Todo priority: "
+    })
     toDOModBtn.addEventListener("click",(e)=>{
         e.preventDefault()
-        projectDialog.close()
+        if(titleModInput.value != ""){
+            todo.title = titleModInput.value
+        }
+        if(describeModInput.value != ""){
+            todo.describe = describeModInput.value
+        }
+        if(dateModInput.value != todo.dueDate){
+            todo.dueDate = dateModInput.value
+        }
+        if(priorityModInput.value != todo.priority){
+            todo.priority = priorityModInput.value
+        }
+        refreshProject(project)
+       
+
+        titleModInput.value =""
+        describeModInput.value = "todo.describe"
+        dateModInput.value = undefined
+        priorityModInput.value = 0
+        titleModCurrentInput.innerText = "Current Todo Title: "
+        describeModCurrentInput.innerText ="Current Todo Desc: "
+        dateModCurrentInput.innerText = "Current Todo Date: "
+        priorityModCurrentInput.innerText = "Current Todo priority: "
+        toDoModDialog.close()
     },once
     )
 }
